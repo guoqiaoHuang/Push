68e5232e829d61daeaed2146217f7b66c3cfd8e5
ydcq <ydcq@ydcqdeMac-mini.local>
2015-01-15 14:06:01 +0800

    Initial Commit

---
+++ PushDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PushDemo
+//
+//  Created by ydcq on 15/1/15.
+//  Copyright (c) 2015年 SJYH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PushDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PushDemo
+//
+//  Created by ydcq on 15/1/15.
+//  Copyright (c) 2015年 SJYH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
